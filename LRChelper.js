86abb39fb9c827dcd556da8052be02697907d0e5
nicrobichaud <52426236+nicrobichaud@users.noreply.github.com>
2020-01-14 17:27:20 -0400

    Update LRChelper.js

--- LRChelper.js
+++ LRChelper.js
@@ -1,1 +1,12 @@
-document.getElementById("menu_rollcall").style.backgroundColor = "yellow";
+//get tray
+var tray = document.getElementsByClassName("mainnav-toggle")[0];
+if (tray.getAttribute("title")==="Expand"){
+  tray.style.backgroundColor = "yellow";
+}
+
+var rc = document.getElementById("menu_rollcall");
+rc.style.backgroundColor = "yellow";
+rc.children[1].children[1].style.backgroundColor = "yellow";
+
+var fg = document.getElementsByClassName("form-group");
+fg[fg.length-1].style.backgroundColor = "yellow";
